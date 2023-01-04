96f2fca863a56f72afe86d27d6cb235d7939455d
Davi <davimoreiraasantoss@gmail.com>
2023-01-03 22:52:05 -0300

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const { Person } = require('./person')
+const person = new Person("Davi") 
+console.log(person.sayMyName())
+
+//Para executar um arquivo com o node basta digitar no terminal: node (nome do arquivo)
