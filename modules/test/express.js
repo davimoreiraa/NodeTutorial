4e0ef65e7bc3dde797ca9b9b3796fe3b557eda7e
Davi <davimoreiraasantoss@gmail.com>
2023-01-06 16:19:45 -0300

    update

--- modules/test/express.js
+++ modules/test/express.js
@@ -3,34 +3,31 @@ const UserModel = require('../../src/models/user.model')
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/home", (req, res) => {
     res.contentType('application/html')
     res.status(200).send("<h1>Hello world</h1>")
 })
 
-app.get("/users", (req,res) => {
-    const users = [
-        {
-            name: 'User1',
-            password: '12345'
-        },
-        {
-            name: 'User2',
-            password: '098765'
-        }
-    ]
+app.get("/users", async (req,res) => {
+    try {
+        const users = await UserModel.find({})
+        res.status(200).json(users)
+    }
 
-    res.status(200).json(users)
+    catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 app.post('/users', async (req, res) => {
-
     try {
         const user = await UserModel.create(req.body)
         res.status(201).json(user)
     }
 
-    catch(error) {
+    catch (error) {
         res.status(500).send(error.message)
     }
     //code status 201 diz que o registro foi criado com sucesso
